76e1599d2dfe99e04a6f7850b10c5bd71d9c34bc
kusumakrish <80766178+kusumakrish@users.noreply.github.com>
2022-06-03 20:37:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,68 @@
-//Difference between “ == “ and “ === “ operators.
+// // Difference between == and ===
+
+//These are comparison operators
+
+let num1 = 2;
+let num2 = "2";
+
+// // == : checks only the both the values are same or not.
+
+console.log(num1 == num2)//true
+
+// //=== : checks both datatypes and values are same or not.
+
+console.log(num1 === num2)//true
+
+// //2. What is the spread operator?
+// //A.  Spread operator will define as ... it allows iterable expand in place where 0+ arguments are expected. it will use for arrays where more than one element
+
+// let fruits = ["apple","mango","banana"];
+// console.log(...fruits);
+
+// //3. What are the differences between var, let and const?
+// // var: var has a global scope. var can be intilized in one line and can be assigned in another line. var can be redeclered and reintilized.
+// // Let: let has a local scope. let can be intilized in one line and can be assigned in another line. let cannot be reclered but can be re intilized.
+// // const: const has a local scope. const cannot be relintilized and cannot be redeclered.
+{
+    var i=0;
+    let s=2;
+    const c = 4;
+}
+console.log(i);//0
+console.log(s);//Reference Error : s Not defined
+console.log(c);//Reference Error : c Not defined
+
+// //4. What is execution context?
+// //execution context : execution context has two phases 1. creation phase 2. execution phase
+// //creation phase : In this phase intrepreter creates the memory for the variables and creates a function expression.
+// //Execution phase : In this phase interpreter will executes the code that currently running. it allocates the actual values to the variables by removing undefined.
+
+// //5. What is meant by first class functions?
+// //first class function : the function that can be passed as value or return the function as a value. This unique feature is called FCF's first class functions or first class citizens.
+
+function sayHi(friend){
+    console.log("Hi",friend);
+}
+
+function friends(friend,FCF){
+    FCF(friend);
+}
+
+friends("bala",sayHi);
+
+//Here sayHi is the First class function / first class citizens.
+//Here friends is the higher order function.
  
-//these are comparison operators
+// //6. What are closures?
+// //closures are functions that references variables in the outer scope from its inner scope.
+//  let outerfn = () => {
+//      let count=0;
+//      return innerfn = () => {
+//          console.log(count++);
+//      }
 
-//"=="is used to compare the values
-
-console.log(a==a);
-
-//"===" is used to compare the value and database.
-console.log(a==="a");
-
-//What is the spread operator?
-
-//it is ES6 new set operators.It takes an array and expands it into individual elements.
-//spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
-//eg:
-
-let js = ["function","array","object"];
-console.log(...js);
-
-
-//What are the differences between var, let and const?
-
-//var - *It is a Global scope
-//      *It can be reassigned or redeclared.
-//      *It can be modified later.
-
-var a = 2;
-console.log(a);
-a = 7;
-
-//let - *It is a block scope
-//      *It cannot be redeclared or reassigned.
-//      *It can be modified later.
-
-let name1; 
-name1 ="supriya";
-console.log(name1);
-name1 = "prabu"
-
-
-//const - *It is a block scope.
-//      *It cannot be redeclared or reassigned.
-//      *It cannot be modified later.
-
-const city = "coimbatore";
-console.log(city);
-//What is execution context
-
-//it contains two phases : creation phase and execution phase: creation phase: allocate memory to variables & function definitions are allocated to name of functions , exact values are not assigned to variables instead default value undefined is assigned to all variables.
-//execution phase: exact value for the variable will be overwritten and undefined will be removed. function body is now executed once function is called.
-//after execution phase both global & function execution context gets removed from call stack.
-
-//What is meant by first class functions
-//
-//n Javascript, functions are first class objects. First-class functions means when functions in that language are treated like any other variable.
-//Function which gets treated as a variable.It can be passed as an argument to other functions,can be returned by another function and can be assigned as a value to a variable.
-//First class function or first class citizen.
-//the function that can be passed as value or return the function as a value. This unique feature is called FCF.
-let g = function(param){ //HOF
-    console.log("i am fn g");
-    console.log(param);
-    param();//callback fn
-}
-
-function h(){ //FCF
-    console.log("i am fn h");
-}
-
-g(h);// h is a function passed as an argument.
-
-
-//What are closures?
-//closures - closure is not something that we create manually or explicitly it happens automatically in certain situation which we need to recognize.
-//closure makes a function remember all the variable that existed at the function birthplace initially.
-//any function always has access to variable environment of the execution context in which the function was created.
-
-const ticketBooking = function(){
-    let passengerCount = 0;
-
-    return function(){
-        passengerCount++;
-        console.log(`the passenger count is ${passengerCount}`)
-    };
-};
-
-const bookie = ticketBooking()
-bookie();
-bookie();
-bookie();
-bookie();
+//  }
+// let sk = outerfn();
+//  sk();
+//  sk();
+//  sk();
